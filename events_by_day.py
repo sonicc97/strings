7a2e9ae023e13b8e5bbb893e392ad7da1a57572b
Sonic <142159825+sonicc97@users.noreply.github.com>
2024-01-04 19:42:05 +0100

    Add files via upload

---
+++ events_by_day.py
@@ -0,0 +1,9 @@
+def show_programme(day):
+    for event in day:
+        print(f'Today: {event}')
+
+monday = ["Swan Lake", "Ravel - Piano Concerto"]
+tuesday = ["La Boheme"]
+
+show_programme(monday)
+show_programme(tuesday)
